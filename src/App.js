54c61f93627df8928e694835b429ece80ba7f776
Nguyen Duong <muoi07052001@gmail.com>
2023-03-22 17:17:50 +0700

    Add new post component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Home from './pages/home/Home';
 import Single from './pages/single/Single';
 import TopBar from './components/topbar/TopBar';
+import Write from './components/write/Write';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 }
