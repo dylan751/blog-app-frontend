67dc1811dab6c76b24408f979e438b083e954705
Nguyen Duong <muoi07052001@gmail.com>
2023-03-22 19:57:28 +0700

    User Settings Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Home from './pages/home/Home';
 import Single from './pages/single/Single';
 import TopBar from './components/topbar/TopBar';
 import Write from './components/write/Write';
+import Settings from './pages/settings/Settings';
 
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Settings />
     </>
   );
 }
