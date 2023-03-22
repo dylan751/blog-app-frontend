22c3d4f86c1c307e2da08400734fe667c0ca888d
Nguyen Duong <muoi07052001@gmail.com>
2023-03-22 20:23:01 +0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import Write from './components/write/Write';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const user = false;
   return (
-    <>
+    <Router>
       <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
       <Register />
-    </>
+    </Router>
   );
 }
 
